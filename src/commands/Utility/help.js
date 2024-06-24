712bc7eea336d7df3470096c2bf79a8ee7022a31
Sylvee <79902941+notsylvee@users.noreply.github.com>
2024-06-24 17:20:52 -0400

    updated /help and organization

--- src/commands/Utility/help.js
+++ src/commands/Utility/help.js
@@ -16,7 +16,11 @@ module.exports = {
     .addSubcommand(command =>
         command
         .setName('emote')
-        .setDescription('Get a list of all emote commands')),
+        .setDescription('Get a list of all emote commands'))
+    .addSubcommand(command =>
+        command
+        .setName('fun')
+        .setDescription('Get a list of all fun commands')),
     async execute (interaction, client) {
 
         const command = interaction.options.getSubcommand();
@@ -59,45 +63,58 @@ module.exports = {
         }
 
         switch (command) {
+            case 'fun':
+            
+            const funembed = new EmbedBuilder()
+            .setColor("Orange")
+            .setTitle("Fun commands")
+            .addFields({ name: "/roll (d4, d6, d8, d10, d12, d20, d100)", value: "Roll some dice" })
+            .setFooter({ text: "Fun commands" })
+            .setTimestamp()
+
+            await interaction.reply({ embeds: [funembed] })
+        }
+
+        switch (command) {
             case 'emote':
             
             const useremoteembed = new EmbedBuilder()
             .setColor("Fuchsia")
             .setTitle("User emote commands")
-            .addFields({ name: "/bite", value: "Bite someone!" })
-            .addFields({ name: "/cuddle", value: "Cuddle someone!" })
-            .addFields({ name: "/holdhands", value: "Hold someones hand!" })
-            .addFields({ name: "/hug", value: "Give someone a hug!" })
-            .addFields({ name: "/kick", value: "Kick someone!" })
-            .addFields({ name: "/kill", value: "Kill someone!" })
-            .addFields({ name: "/kiss", value: "Give someone a kiss!" })
-            .addFields({ name: "/lick", value: "Lick someone!" })
-            .addFields({ name: "/pat", value: "Pat someone!" })
-            .addFields({ name: "/poke", value: "Poke someone!" })
-            .addFields({ name: "/slap", value: "Slap someone!" })
-            .addFields({ name: "/tickle", value: "Tickle someone!" })
-            .setFooter({ text: "User emote commands" })
+            .addFields({ name: "bite", value: "Bite someone!" })
+            .addFields({ name: "cuddle", value: "Cuddle someone!" })
+            .addFields({ name: "holdhands", value: "Hold someones hand!" })
+            .addFields({ name: "hug", value: "Give someone a hug!" })
+            .addFields({ name: "kick", value: "Kick someone!" })
+            .addFields({ name: "kill", value: "Kill someone!" })
+            .addFields({ name: "kiss", value: "Give someone a kiss!" })
+            .addFields({ name: "lick", value: "Lick someone!" })
+            .addFields({ name: "pat", value: "Pat someone!" })
+            .addFields({ name: "poke", value: "Poke someone!" })
+            .addFields({ name: "slap", value: "Slap someone!" })
+            .addFields({ name: "tickle", value: "Tickle someone!" })
+            .setFooter({ text: "These commands start with /useremote" })
             .setTimestamp()
     
             const selfemoteembed = new EmbedBuilder()
             .setColor("Fuchsia")
             .setTitle("Self emote commands")
-            .addFields({ name: "/cry", value: "Sends a crying gif" })
-            .addFields({ name: "/dance", value: "Sends dancing gif!" })
-            .addFields({ name: "/eat", value: "Sends a an eating gif!" })
-            .addFields({ name: "/giggle", value: "Sends a giggling gif!" })
-            .addFields({ name: "/happy", value: "Sends a happy gif!" })
-            .addFields({ name: "/mad", value: "Sends a mad gif!" })
-            .addFields({ name: "/plead", value: "Sends a pleading gif!" })
-            .addFields({ name: "/pout", value: "Sends a pouting gif!" })
-            .addFields({ name: "/sad", value: "Sends a sad gif" })
-            .addFields({ name: "/shy", value: "Sends a shy gif!" })
-            .addFields({ name: "/sleep", value: "Sends a sleeping gif!" })
-            .addFields({ name: "/spin", value: "Sends a spinning gif!" })
-            .addFields({ name: "/suicide", value: "Commit suicide" })
-            .addFields({ name: "/tired", value: "Sends a tired gif!" })
-            .addFields({ name: "/wave", value: "Sends a waving gif!" })
-            .setFooter({ text: "Self emote commands" })
+            .addFields({ name: "cry", value: "Sends a crying gif" })
+            .addFields({ name: "dance", value: "Sends dancing gif!" })
+            .addFields({ name: "eat", value: "Sends a an eating gif!" })
+            .addFields({ name: "giggle", value: "Sends a giggling gif!" })
+            .addFields({ name: "happy", value: "Sends a happy gif!" })
+            .addFields({ name: "mad", value: "Sends a mad gif!" })
+            .addFields({ name: "plead", value: "Sends a pleading gif!" })
+            .addFields({ name: "pout", value: "Sends a pouting gif!" })
+            .addFields({ name: "sad", value: "Sends a sad gif" })
+            .addFields({ name: "shy", value: "Sends a shy gif!" })
+            .addFields({ name: "sleep", value: "Sends a sleeping gif!" })
+            .addFields({ name: "spin", value: "Sends a spinning gif!" })
+            .addFields({ name: "suicide", value: "Commit suicide" })
+            .addFields({ name: "tired", value: "Sends a tired gif!" })
+            .addFields({ name: "wave", value: "Sends a waving gif!" })
+            .setFooter({ text: "These commands start with /selfemote" })
             .setTimestamp()
 
             const button = new ActionRowBuilder()
