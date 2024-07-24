929cfb9963d2ba57c4695ff9b6ec0ea5e7749e9e
Sylvee <79902941+notsylvee@users.noreply.github.com>
2024-07-23 21:53:15 -0400

    fixed greetings responses

--- src/events/greetings.js
+++ src/events/greetings.js
@@ -8,29 +8,29 @@ module.exports = {
         const response = msg[Math.floor(Math.random() * msg.length)];
 
         if (message.content.toLowerCase().startsWith("hi") && message.content.toLowerCase().endsWith("i")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("hello") && message.content.toLowerCase().endsWith("hello")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("hey") && message.content.toLowerCase().endsWith("hey")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("howdy") && message.content.toLowerCase().endsWith("howdy")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("hai") && message.content.toLowerCase().endsWith("hai")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("hewwo") && message.content.toLowerCase().endsWith("hewwo")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("whats up") && message.content.toLowerCase().endsWith("whats up")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("yo") && message.content.toLowerCase().endsWith("yo")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("hallo") && message.content.toLowerCase().endsWith("hallo")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("sup") && message.content.toLowerCase().endsWith("sup")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("aloha") && message.content.toLowerCase().endsWith("aloha")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         } else if (message.content.toLowerCase().startsWith("greetings") && message.content.toLowerCase().endsWith("greetings")) {
-            message.channel.send(`-# ${response} ${message.author.displayName}`)
+            message.channel.send(`-# ${response} ${message.member.displayName}`)
         }
     }
 }
