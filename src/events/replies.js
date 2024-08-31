1e67d75dcfce04c02279162a0ab0db3a440e49d1
Sylvee <79902941+notsylvee@users.noreply.github.com>
2024-08-31 18:52:36 -0400

    added an update message

--- src/events/replies.js
+++ src/events/replies.js
@@ -145,7 +145,8 @@ module.exports = {
     if (
       message.content.toLowerCase().includes("i love you sylvbot") ||
       message.content.toLowerCase().includes("sylvbot i love you") ||
-      message.content.toLowerCase().includes("<@953885359668871238> i love you")
+      message.content.toLowerCase().includes("<@953885359668871238> i love you") ||
+      message.content.toLowerCase().includes("i love you <@953885359668871238>")
     ) {
       message.channel.send(`${lovemsg} ${lovename}!`);
     }
