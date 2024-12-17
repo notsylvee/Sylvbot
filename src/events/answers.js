e76e075ce9847544bc7192f0e9837d55bb5ee429
Sylvee <79902941+notsylvee@users.noreply.github.com>
2024-12-16 22:23:58 -0500

    added more answers for questions

--- src/events/answers.js
+++ src/events/answers.js
@@ -6,12 +6,34 @@ module.exports = {
       const msg = [
         `yes`,
         `no`,
-        `idk :P`,
-        `mayyybe :3`,
+        `mayyybe`,
+        `idk`,
+        `maybe`,
+        `perhaps`,
+        `perchance`,
+        `mayhaps`,
+        `nuh uh`,
+        `yeah`,
+        `nah`,
+        `NO!`,
+        `i dont know`,
+        `yuh huh`,
+        `YASSSS`
       ];
       const response = msg[Math.floor(Math.random() * msg.length)];
+      const faces = [` `, ` `, ` `, ` `, ` `, ` `, ` `, ` `,
+        `:3`,
+        `>.<`,
+        `>:(`,
+        `uwu`,
+        `:(`,
+        `:)`,
+        `;3`,
+        `;)`,
+      ]
+      const face = faces[Math.floor(Math.random() * faces.length)];
   
       if (message.content.toLowerCase().startsWith("sylvbot") && message.content.toLowerCase().endsWith("?"))
-        {message.channel.send(`${response}`)}
+        {message.channel.send(`${response} ${face}`)}
     },
   };
