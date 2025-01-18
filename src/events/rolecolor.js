c6d2c6226bf6667c21c54f652a0ba8a3c2a11606
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-01-17 21:52:59 -0500

    deleted react.js

--- src/events/rolecolor.js
+++ src/events/rolecolor.js
@@ -9,11 +9,11 @@ module.exports = {
       if (message.author.id === "743779456530907216") roleId = "1292303341594087574"; /* blozzy */
       if (message.author.id === "431220802797568001") roleId = "1251737416038088774"; /* tharen */
       if (message.author.id === "529405805686947843") roleId = "1256325872277786656"; /* pluto */
+      if (message.author.id === "788222689126776832") roleId = "1330004407449948180"; /* valk */
       if (message.author.id === "1137552202265796638") roleId = "1248506339496300608"; /* denny */
       if (message.author.id === "457353908524941335") roleId = "1264765642448572569"; /* otter */
       if (message.author.id === "416799608744706048") roleId = "1252437304803590187"; /* kelp */
       if (message.author.id === "596356028187148309") roleId = "1269577220662235147"; /* stelle */
-      if (message.author.id === "451866995760168965") roleId = "1303596064909168640"; /* trevor */
 
       if (roleId === "0") return;
       const role = message.guild.roles.cache.get(roleId);
