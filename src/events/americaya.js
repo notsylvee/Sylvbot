bf17fce677c184d1389be6bf9e4d8d83e85c9c4a
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-02-15 23:56:01 -0500

    moved americaya.js to sylvcord bot

--- src/events/americaya.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "messageCreate",
-  async execute(message) {
-    if (!message.guild) return;
-    if (message.channel.id !== `1221908615334461460`) return;
-    if (message.content !== ("Hallo :D")) {message.delete()}
-  },
-};
