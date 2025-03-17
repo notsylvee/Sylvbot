5714349bf9cae726e9a8ed1f0a1875f2794a7b7e
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-17 01:47:18 -0400

    added more ways to say hpd to bot

--- src/events/bday.js
+++ src/events/bday.js
@@ -8,7 +8,17 @@ module.exports = {
       if (message.author.id === "529405805686947843") name = "grandma";
       if (message.author.id === "431220802797568001") name = "step parent";
 
-      if (message.content.toLowerCase().includes("happy birthday sylvbot") || message.content.toLowerCase().includes("happy bday sylvbot"))
-        {message.channel.send(`Thank you ${name}!`)}
+      if (message.content.toLowerCase().includes("happy birthday sylvbot"))
+        {message.channel.send(`Thank you ${name}!`)};
+      if (message.content.toLowerCase().includes("happy bday sylvbot"))
+        {message.channel.send(`Thank you ${name}!`)};
+      if (message.content.toLowerCase().includes("happy birthday <#953885359668871238>"))
+        {message.channel.send(`Thank you ${name}!`)};
+      if (message.content.toLowerCase().includes("happy bday <#953885359668871238>"))
+        {message.channel.send(`Thank you ${name}!`)};
+      if (message.content.toLowerCase().includes("<#953885359668871238> happy birthday"))
+        {message.channel.send(`Thank you ${name}!`)};
+      if (message.content.toLowerCase().includes("<#953885359668871238> happy bday"))
+        {message.channel.send(`Thank you ${name}!`)};
     },
   };
