29f90f6a7f11ebfd21763e1e96bdfe09a2cfe8b4
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-17 01:51:06 -0400

    added 2 responses to hpd messages

--- src/events/bday.js
+++ src/events/bday.js
@@ -8,17 +8,20 @@ module.exports = {
       if (message.author.id === "529405805686947843") name = "grandma";
       if (message.author.id === "431220802797568001") name = "step parent";
 
+      const responses = ["Thank you", "Thanks"]
+      const response = responses[Math.floor(Math.random() * responses.length)];
+
       if (message.content.toLowerCase().includes("happy birthday sylvbot"))
-        {message.channel.send(`Thank you ${name}!`)};
+        {message.channel.send(`${response} ${name}!`)};
       if (message.content.toLowerCase().includes("happy bday sylvbot"))
-        {message.channel.send(`Thank you ${name}!`)};
+        {message.channel.send(`${response} ${name}!`)};
       if (message.content.toLowerCase().includes("happy birthday <#953885359668871238>"))
-        {message.channel.send(`Thank you ${name}!`)};
+        {message.channel.send(`${response} ${name}!`)};
       if (message.content.toLowerCase().includes("happy bday <#953885359668871238>"))
-        {message.channel.send(`Thank you ${name}!`)};
+        {message.channel.send(`${response} ${name}!`)};
       if (message.content.toLowerCase().includes("<#953885359668871238> happy birthday"))
-        {message.channel.send(`Thank you ${name}!`)};
+        {message.channel.send(`${response} ${name}!`)};
       if (message.content.toLowerCase().includes("<#953885359668871238> happy bday"))
-        {message.channel.send(`Thank you ${name}!`)};
+        {message.channel.send(`${response} ${name}!`)};
     },
   };
