aee16f9287f27e16802913acc11844900bfe7d4c
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-04-09 02:48:16 -0400

    +status.js

---
+++ src/events/status.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "messageCreate",
+    async execute(message) {
+        if (!message.channel.id === `1359404696149819412`) return;
+
+        client.user.setPresence({
+              status: "idle",
+              activities: [
+                {
+                  type: ActivityType.Custom,
+                  name: "customstatus",
+                  state: "<3",
+                },
+              ],
+            });
+    }
+}
