2939cb63aca3ab4aaaa307c9c803c514edfd4faf
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-05-11 18:28:04 -0400

    fixed role ids

--- src/events/rolecolor.js
+++ src/events/rolecolor.js
@@ -5,9 +5,9 @@ module.exports = {
       if (message.guild.id !== "1217915488332550218") return;
 
       let roleId = "0";
-      if (message.author.id === "1018686464000807003") roleId = "1217915644968701962"; /* sylvee */
+      if (message.author.id === "1018686464000807003") roleId = "1369875325663842334"; /* sylvee */
       if (message.author.id === "743779456530907216") roleId = "1292303341594087574"; /* blozzy */
-      if (message.author.id === "431220802797568001") roleId = "1251737416038088774"; /* tharen */
+      if (message.author.id === "431220802797568001") roleId = "1370262148923654225"; /* tharen */
       if (message.author.id === "788222689126776832") roleId = "1330004407449948180"; /* valk */
       if (message.author.id === "1137552202265796638") roleId = "1248506339496300608"; /* denny */
       if (message.author.id === "457353908524941335") roleId = "1264765642448572569"; /* otter */
