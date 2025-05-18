e76a080e8417be6b26adf7f1231a3c3dd0a02b59
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-05-17 21:33:42 -0400

    for timmy

--- src/events/chatresponses.js
+++ src/events/chatresponses.js
@@ -124,9 +124,10 @@ module.exports = {
     if (message.content.toLowerCase() === "type shit") {message.channel.send("-# shit")}
     if (message.content.toLowerCase() === "kys") {message.channel.send("-# keep yourself safe")}
     if (message.content.toLowerCase() === "aw man") {message.channel.send("-# creeper! aw man")}
+    if (message.content.toLowerCase().includes("timmy")) {message.channel.send("-# For Timmy!")}
     if (message.content.toLowerCase() === "smh my head") {message.channel.send("-# shaking my head my head")}
+    if (message.content.toLowerCase() === "i dont give a gaf") {message.channel.send("-# i dont give a give a fuck")}
     if (message.content.toLowerCase().includes("massive")) {message.channel.send("-# you know what else is massive?")}
-    if (message.content.toLowerCase().includes("i dont give a gaf")) {message.channel.send("-# i dont give a give a fuck")}
 
     //MARK: secret
     const secretrepliesmath = Math.random() * 5000;
