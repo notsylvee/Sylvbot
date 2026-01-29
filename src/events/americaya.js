dd6f77e166c9eb14b91742820e42fe676303440e
Sylvee <sylveevaleriehaven@gmail.com>
2026-01-29 00:33:00 -0500

    america ya fix

--- src/events/americaya.js
+++ src/events/americaya.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "messageCreate",
-  async execute(message, client) {
+  async execute(message) {
     if (message.channel.id !== `1466301788478247076`) return;
-    if (message.content !== "America ya :D") {message.delete}
+    if (message.content !== "Hallo :D") {message.delete()}
   }
 }
